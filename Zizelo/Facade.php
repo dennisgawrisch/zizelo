7d64d8e7b0f01bd0369f55442fe43d94c8bfcfcc
Denys Popov <besisland@besisland.name>
2011-01-02 05:14:54 +0200

    reintroduced min relevance

--- Zizelo/Facade.php
+++ Zizelo/Facade.php
@@ -2,8 +2,9 @@
 class Zizelo_Facade {
     const RELEVANCE_TEXT    = 1.0;
     const RELEVANCE_HASH    = 0.5;
+    const RELEVANCE_MINIMAL = 0.3;
 
-    const FREQUENCY_MAX     = 0.03;
+    const FREQUENCY_MAX     = 0.01;
 
     private static $default_storage;
     private static $default_analyzer;
@@ -125,7 +126,9 @@ class Zizelo_Facade {
 
         foreach ($documents as $document_id => $document) {
             $relevance = ($document["text"] * self::RELEVANCE_TEXT + $document["hash"] * self::RELEVANCE_HASH) / count($words);
-            $matches[$document_id] = $relevance;
+            if ($relevance >= self::RELEVANCE_MINIMAL) {
+                $matches[$document_id] = $relevance;
+            }
             if ($relevance >= $perfect_match_relevance) {
                 $perfect_match_ids []= $document_id;
             }
